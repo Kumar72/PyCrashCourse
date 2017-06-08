1a2ecb9176e8de39fd97d8c6022994616025dec6
Kumar72 <vichandan@gmail.com>
2017-06-08 18:25:26 -0400

    tested out python loop

--- Introduction/chapter_4.py
+++ Introduction/chapter_4.py
@@ -1,1 +1,22 @@
 # Working with Lists
+
+# EX: 4.1
+pizzas = ['Veggie Lovers', 'Buffalo Chicken', 'Meat Lovers', 'Mediterranean']
+for pizza in pizzas:
+    print(pizza.title())
+print('I can eat pizza for days!\n')
+# the colon indicated the start of a for loop
+# indent only when you are suppose to, as it is part of certain syntax ex. for loop
+# main cause of logical errors in python
+
+# EX: 4.2
+animals = ['lion', 'leopard', 'tiger', 'cheetah']
+for cat in animals:
+    print('A '+cat.title()+' would make a nice pet.')
+print('Which cat would you pick as your pet?\n')
+
+# range() function
+for value in range(1,20):
+    print(value)
+
+
